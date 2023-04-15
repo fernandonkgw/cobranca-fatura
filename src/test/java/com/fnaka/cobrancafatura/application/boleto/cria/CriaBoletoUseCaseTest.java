35dd142c0b055c5c49d04a98cd791cf38db244db
Fernando Nakagawa <fernandonkgw@gmail.com>
2023-04-15 12:13:04 -0300

    feat(boleto): cria boleto use case com convenio nulo lanca exception

--- src/test/java/com/fnaka/cobrancafatura/application/boleto/cria/CriaBoletoUseCaseTest.java
+++ src/test/java/com/fnaka/cobrancafatura/application/boleto/cria/CriaBoletoUseCaseTest.java
@@ -4,6 +4,8 @@ import com.fnaka.cobrancafatura.UseCaseTest;
 import com.fnaka.cobrancafatura.domain.boleto.Boleto;
 import com.fnaka.cobrancafatura.domain.boleto.BoletoGateway;
 import com.fnaka.cobrancafatura.domain.boleto.BoletoStatus;
+import com.fnaka.cobrancafatura.domain.exceptions.DomainException;
+import com.fnaka.cobrancafatura.domain.validation.ErrorCode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
@@ -56,6 +58,25 @@ class CriaBoletoUseCaseTest extends UseCaseTest {
         Assertions.assertEquals(expectedStatus, boleto.getStatus());
         Assertions.assertNotNull(boleto.getCriadoEm());
         Assertions.assertNotNull(boleto.getAtualizadoEm());
+    }
+
+    @Test
+    void givenAInvalidNullConvenio_whenCallsCriaBoleto_shouldThrowsException() {
+        // given
+        final var expectedNumeroTituloCliente = "12345678901234567890";
+        final var expectedErrorMessage = "'convenio' should not be null";
+        final var expectedErrorCode = ErrorCode.CFA_001;
+
+        final var command = CriaBoletoCommand.with(null, expectedNumeroTituloCliente);
+
+        // when
+        final var actualException = Assertions.assertThrows(
+                DomainException.class, () -> useCase.execute(command)
+        );
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, actualException.getFirstError().message());
+        Assertions.assertEquals(expectedErrorCode, actualException.getFirstError().code());
     }
 }
 
